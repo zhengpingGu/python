173267b8b02d21d8bd1619c250c617bddfc9603d
guzp <guzp@CIT000094NB.ciandt.cn>
2014-06-17 16:03:18 +0800

    hello python

---
+++ MyPythonProject/src/mypythonproject.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="guzp"
+__date__ ="$2014-6-17 16:00:33$"
+
+if __name__ == "__main__":
+    print "Hello World"
